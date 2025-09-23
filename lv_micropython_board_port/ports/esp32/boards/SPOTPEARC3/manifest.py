b8c748d900fdde0a85b59ef33b5f3873dae18c43
Lucien Murray-Pitts <lucienmp_antispam@yahoo.com>
2025-09-24 01:19:59 +0900

    Adding updated st7735 tft driver for 128x128 resolution

--- lv_micropython_board_port/ports/esp32/boards/SPOTPEARC3/manifest.py
+++ lv_micropython_board_port/ports/esp32/boards/SPOTPEARC3/manifest.py
@@ -4,8 +4,9 @@
 include("$(PORT_DIR)/boards/manifest.py")
 include("$(MPY_DIR)/user_modules/lv_binding_micropython/ports/esp32")
 
-# Add specific driver
-module("st77xx.py", base_path="$(MPY_DIR)/user_modules/lv_binding_micropython/driver/generic")
+# Add specific driver: Fixed up for (128,128) resolution, now inclulded in modules
+#module("st77xx.py", base_path="$(MPY_DIR)/user_modules/lv_binding_micropython/driver/generic")
+#
 
 # Modules for ESP32C3 1.44 MiniTV
 freeze("./modules")
